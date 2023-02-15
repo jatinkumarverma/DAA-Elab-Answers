949c18fb271043da30fe4f15d7be2b211f22a5d6
JATIN KUMAR VERMA <82764118+jatinkumarverma@users.noreply.github.com>
2023-02-15 09:05:08 +0530

    Create maari.cpp

---
+++ maari.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int p,d,m,s;
+    cin>>p>>d>>m>>s;
+    int sum=0,count=0;
+    while(sum<=s) 
+    { if(p<m) sum+=m;
+    else sum+=p;
+    p-=d;
+    count++;
+    }
+    cout<<count-1;
+    return 0;
+    cout<<"while(p<=s)";
+}

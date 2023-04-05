44d275cf04675d6df1bed0015b369d2cd6af6f1f
JATIN KUMAR VERMA <82764118+jatinkumarverma@users.noreply.github.com>
2023-04-06 00:36:27 +0530

    Create pyramid.cpp

---
+++ pyramid.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    int t,l=1;
+    cin>>t;
+
+    while(t--){
+
+        cout<<"Process #"<<l<<":"<<endl;
+       int n;
+        cin>>n;
+
+       for(int i=1;i<n+1;i++){
+           cout<<i<<" "<<i<<endl;
+       }
+       l++;
+    }
+
+    return 0;
+    cout<<"for(j=row;j>=0;j--)";
+
+}

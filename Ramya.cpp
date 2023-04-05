5d1c9c8f8b86df2e3602f1c04a48198b10351066
JATIN KUMAR VERMA <82764118+jatinkumarverma@users.noreply.github.com>
2023-04-06 00:23:54 +0530

    Create Ramya.cpp

---
+++ Ramya.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char a;
+    cin>>a;
+    if(a==97)cout<<"YES";
+    else if(a==71)cout<<"NO";
+    else if(a<72)cout<<"NO";
+    else cout<<"YES";
+    return 0;
+    cout<<"string cin>>t";
+}
